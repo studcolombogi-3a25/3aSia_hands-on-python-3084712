28a93c130732111ae04d789de35feefc5981f65f
studcolombogi-3a25 <stud.colombogi@gdromagnosi.edu.it>
2026-02-26 09:59:11 +0000

    agiungies

--- Ex_Files/02_03_begin/main.py
+++ Ex_Files/02_03_begin/main.py
@@ -1,18 +1,18 @@
-NAMES = ["John", "Paul", "George", "Ringo"]
+NAMES = ["frank", "Paul", "George", "Ringo"]
 AGES = [20, 21, 22, 23]
 
-JOHN = NAMES[0]
+frank = NAMES[0]
 PAUL = NAMES[1]
 
-JOHN_PAUL = NAMES[:2]
-GEORGE_RINGO = NAMES[2:]
-REVERSE = NAMES[::-1]
-EVERY_OTHER = NAMES[::2]
+frank_PAUL = NAMES[:6]
+GEORGE_RINGO = NAMES[4:]
+REVERSE = NAMES[::-2]
+EVERY_OTHER = NAMES[::10]
 
 print(sum(AGES))
 print(min(AGES))
 print(max(AGES))
 
-print(JOHN_PAUL)
+print(frank_PAUL)
 print(GEORGE_RINGO)
 print(REVERSE)
